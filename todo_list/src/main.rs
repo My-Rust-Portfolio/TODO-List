e71b51f9fcf94565d3767495e806223ac49a657c
Gor <gor.hakobyan97@gmail.com>
2026-03-14 21:39:20 +0100

    Added parser to be able to enter data through console

--- todo_list/src/main.rs
+++ todo_list/src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Debug)]
+struct Task {
+    title: String,
+    description: String,
+    completed: bool,
+    index: usize,
 }
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    List,
+    Add { title: String, description: String },
+    Complete { index: usize },
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[command(subcommand)] // Tells clap to parse the subcommand into this field
+    command: Commands,
+}
+
+fn main() {
+    let args = Args::parse();  // Fills from CLI args
+    println!("Parsed: {:?}", args.command);
+
+    let mut tasks: Vec<Task> = Vec::new();
+
+    match args.command {
+        Commands::Add { title, description } => {
+            let task = Task { title, description, completed: false, index: tasks.len() };
+            tasks.push(task);
+            println!("Added: {:?}", tasks.last().unwrap());
+        }
+        Commands::List => {
+            for task in &tasks {
+                println!("{:?}", task);
+            }
+        }
+        Commands::Complete { index} => {
+            if (index >= tasks.len()) {
+                eprintln!("Error: No task with index {}", index);
+                return;
+            }
+
+            tasks.iter_mut().find(|t| t.index == index).map(|t| t.completed = true);
+            println!("Completed task with index: {}", index);
+        }
+    }
+}
+
+
