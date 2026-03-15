f995e5b48feaef284f86cf361d07376f866ab817
Gor <gor.hakobyan97@gmail.com>
2026-03-15 14:45:34 +0100

    Displaying tasks in the window

--- todo_list/src/bin/gui.rs
+++ todo_list/src/bin/gui.rs
@@ -34,10 +34,11 @@ impl TodoApp {
 impl eframe::App for TodoApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello World again!"); 
             ui.heading("My Todo List");
 
             for task in &self.app_state.tasks {
+                ui.horizontal(|ui| {
+                    let status = if task.completed { "X" } else { "   " };
                     ui.label(format!("[{}] {}: {}", status, task.title, task.description));
                 });
             }
