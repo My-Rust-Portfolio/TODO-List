f1e957526519284b895b5b9a35f12d4041897dc4
Gor <gor.hakobyan97@gmail.com>
2026-03-15 12:47:13 +0100

    Fixed tests stability, updated readme,

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Todo List
+A program allowing users to manage their tasks. Supports addition, completion, deleteion and listing of the tasks.  
 
 ## Usage
-cargo run -- add "item" "description" (eg. add "milk" "buy")
-cargo run -- list
-cargo run -- complete #id (eg. complete 1)
-cargo run -- delete #id (eg. delete 1)
+cargo run -- add "item" "description" (eg. add "milk" "buy")  
+cargo run -- list  
+cargo run -- complete #id (eg. complete 1)  
+cargo run -- delete #id (eg. delete 1)  
+cargo test  
--- todo_list/src/lib.rs
+++ todo_list/src/lib.rs
@@ -8,7 +8,7 @@ pub mod command_handler;
 mod tests {
     use crate::data::{AppState, Task};
     use crate::persistence::{delete_state_file_name, load_state_from_file_name, save_state_to_file_name, state_file_name};
-    use crate::command_handler::{self, Commands, handle_command};
+    use crate::command_handler::{Commands, handle_command};
     
     #[test]
     fn test_task_new() {
@@ -24,7 +24,7 @@ mod tests {
         let task = Task::new("Milk", "Groceries", state.next_index);
         state.tasks.push(task);
         state.next_index += 1;
-        let file_name = "test_state.json";
+        let file_name = "test_state_1.json";
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
         let loaded_state = load_state_from_file_name(file_name).expect("Failed to load state");
         assert_eq!(loaded_state.tasks.len(), 1);
@@ -40,7 +40,7 @@ mod tests {
     #[test]
     fn test_app_state_add_command() {
         let mut state = AppState { tasks: vec![], next_index: 0 };
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
         assert_eq!(state.tasks.len(), 1);
         assert_eq!(state.tasks[0].title, "Task 1");
         assert_eq!(state.tasks[0].description, "Description 1");
@@ -49,43 +49,43 @@ mod tests {
     #[test]
     fn test_app_state_complete_command() {
         let mut state = AppState { tasks: vec![], next_index: 0 };
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
-        command_handler::handle_command(&mut state, &Commands::Complete { index: 0 });
+        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        handle_command(&mut state, &Commands::Complete { index: 0 });
         assert_eq!(state.tasks[0].completed, true);
     }
 
     #[test]
     fn test_app_state_delete_command() {
         let mut state = AppState { tasks: vec![], next_index: 0 };
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
-        command_handler::handle_command(&mut state, &Commands::Delete { index: 0 });
+        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        handle_command(&mut state, &Commands::Delete { index: 0 });
         assert_eq!(state.tasks.len(), 0);
     }
 
     #[test]
     fn test_app_state_next_id() {
         let mut state = AppState { tasks: vec![], next_index: 0 };
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 0".to_string(), description: "Description 0".to_string() });
-        let file_name = "test_state.json";
+        handle_command(&mut state, &Commands::Add { title: "Task 0".to_string(), description: "Description 0".to_string() });
+        let file_name = "test_state_2.json";
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
         assert_eq!(state.tasks[1].index, 1);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 2".to_string(), description: "Description 2".to_string() });
+        handle_command(&mut state, &Commands::Add { title: "Task 2".to_string(), description: "Description 2".to_string() });
         assert_eq!(state.tasks[2].index, 2);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        command_handler::handle_command(&mut state, &&Commands::Delete { index: (1) });
+        handle_command(&mut state, &Commands::Delete { index: 1 });
         assert_eq!(state.tasks[1].index, 2);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        command_handler::handle_command(&mut state, &Commands::Add { title: "Task 3".to_string(), description: "Description 3".to_string() });
+        handle_command(&mut state, &Commands::Add { title: "Task 3".to_string(), description: "Description 3".to_string() });
         assert_eq!(state.tasks[2].index, 3);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
         
