6b08a027806f840647f94d4bf23a64abf652b135
Gor <gor.hakobyan97@gmail.com>
2026-03-15 14:43:54 +0100

    Displaying the tasks in the window

--- todo_list/src/bin/gui.rs
+++ todo_list/src/bin/gui.rs
@@ -1,12 +1,11 @@
-use eframe::egui;
 use anyhow::Result;
+use eframe::egui;
 // The GUI can use the exact same data and persistence logic!
 use todo_list::data::AppState;
-use todo_list::persistence::{load_state_file};
+use todo_list::persistence::load_state_file;
 
 fn main() -> Result<(), eframe::Error> {
-    //let state:AppState = load_state_file()?;
-    //println!("GUI loaded {} tasks!", state.tasks.len());
+    let state: AppState = load_state_file().unwrap_or(AppState { tasks: vec![], next_index: 0 });
     
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
@@ -18,25 +17,30 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Todo App",
         options,
-        // Box::new creates a heap allocation (like std::make_unique)
-        Box::new(|_cc| Ok(Box::new(TodoApp {}))),
+        Box::new(|_cc| Ok(Box::new(TodoApp::new(state)))),
     )
 }
 
 struct TodoApp {
+    app_state: AppState,
+}
+
+impl TodoApp {
+    fn new(state: AppState) -> Self {
+        TodoApp { app_state: state }
+    }
 }
 
 impl eframe::App for TodoApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        
-        // CentralPanel takes up the whole window
         egui::CentralPanel::default().show(ctx, |ui| {
-            
-            // Draw a big header
-            ui.heading("Hello World!");
-            
-            // Draw a normal label
             ui.label("Hello World again!"); 
+            ui.heading("My Todo List");
+
+            for task in &self.app_state.tasks {
+                    ui.label(format!("[{}] {}: {}", status, task.title, task.description));
+                });
+            }
         });
     }
 }
