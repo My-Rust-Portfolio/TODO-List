e0eea369656b67f9033169daa02c12abe0cc822b
Gor <gor.hakobyan97@gmail.com>
2026-03-15 17:13:43 +0100

    Added Add Task functionality

--- todo_list/src/bin/gui.rs
+++ todo_list/src/bin/gui.rs
@@ -5,8 +5,11 @@ use todo_list::data::AppState;
 use todo_list::persistence::load_state_file;
 
 fn main() -> Result<(), eframe::Error> {
-    let state: AppState = load_state_file().unwrap_or(AppState { tasks: vec![], next_index: 0 });
-    
+    let state: AppState = load_state_file().unwrap_or(AppState {
+        tasks: vec![],
+        next_index: 0,
+    });
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([500.0, 500.0]) // Width, Height
@@ -23,11 +26,20 @@ fn main() -> Result<(), eframe::Error> {
 
 struct TodoApp {
     app_state: AppState,
+
+    show_add_window: bool,
+    draft_title: String,
+    draft_desc: String,
 }
 
 impl TodoApp {
     fn new(state: AppState) -> Self {
-        TodoApp { app_state: state }
+        TodoApp {
+            app_state: state,
+            show_add_window: false,
+            draft_title: String::new(),
+            draft_desc: String::new(),
+        }
     }
 }
 
@@ -35,25 +47,69 @@ impl eframe::App for TodoApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My Todo List");
+
+            let add_task = ui.button("➕ Add Task");
+            if add_task.clicked() {
+                self.show_add_window = true;
+            }
+
+            ui.separator();
+
             let mut tasks_to_delete: Vec<usize> = vec![];
             for task in &mut self.app_state.tasks {
                 ui.horizontal(|ui| {
-                    let status = if task.completed { "X" } else { "   " };
+                    let status = if task.completed { "✅" } else { "    " };
                     let complete_button = ui.button(status).on_hover_text("Complete task");
                     if complete_button.clicked() {
                         task.completed = !task.completed;
                     }
                     ui.label(format!("{}: {}", task.title, task.description));
 
-                    let delete_button = ui.button("🗑️").on_hover_text("Delete task Permanently");
+                    let delete_button = ui.button("❌").on_hover_text("Delete task Permanently");
                     if delete_button.clicked() {
                         tasks_to_delete.push(task.index);
-                    }                    
+                    }
                 });
             }
 
             for index in tasks_to_delete {
                 self.app_state.tasks.retain(|t| t.index != index);
+            }
+
+            if self.show_add_window {
+                egui::Window::new("Add New Task")
+                    .collapsible(false)
+                    .resizable(false)
+                    .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::new(0.0, 0.0))
+                    .show(ctx, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.label("Title:");
+                            ui.text_edit_singleline(&mut self.draft_title);
+                        });
+
+                        ui.horizontal(|ui| {
+                            ui.label("Description:");
+                            ui.text_edit_singleline(&mut self.draft_desc);
+                        });
+
+                        ui.separator();
+
+                        let create_task_button = ui.button("Create Task");
+                        if create_task_button.clicked() {
+                            let new_task = todo_list::data::Task::new(
+                                &self.draft_title,
+                                &self.draft_desc,
+                                self.app_state.next_index,
+                            );
+                            self.app_state.tasks.push(new_task);
+                            self.app_state.next_index += 1;
+
+                            // Clear drafts and close window
+                            self.draft_title.clear();
+                            self.draft_desc.clear();
+                            self.show_add_window = false;
+                        }
+                    });
             }
         });
     }
