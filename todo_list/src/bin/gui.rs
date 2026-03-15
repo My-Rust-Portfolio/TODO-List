75812e6de5c39aa51bff7caf3d5f6bdbea87b462
Gor <gor.hakobyan97@gmail.com>
2026-03-15 18:12:17 +0100

    Moved main functionality of gui into its own handler

--- todo_list/src/bin/gui.rs
+++ todo_list/src/bin/gui.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use eframe::egui::{self};
 // The GUI can use the exact same data and persistence logic!
 use todo_list::data::AppState;
+use todo_list::gui_handler::TodoApp;
 use todo_list::persistence::load_state_file;
 
 fn main() -> Result<(), eframe::Error> {
@@ -22,108 +23,4 @@ fn main() -> Result<(), eframe::Error> {
         options,
         Box::new(|_cc| Ok(Box::new(TodoApp::new(state)))),
     )
-}
-
-struct TodoApp {
-    app_state: AppState,
-
-    show_add_window: bool,
-    draft_title: String,
-    draft_desc: String,
-}
-
-impl TodoApp {
-    fn new(state: AppState) -> Self {
-        TodoApp {
-            app_state: state,
-            show_add_window: false,
-            draft_title: String::new(),
-            draft_desc: String::new(),
-        }
-    }
-}
-
-impl eframe::App for TodoApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My Todo List");
-
-            let add_task = ui.button("➕ Add Task");
-            if add_task.clicked() {
-                self.show_add_window = true;
-            }
-
-            ui.separator();
-
-            let mut tasks_to_delete: Vec<usize> = vec![];
-
-            for task in &mut self.app_state.tasks {
-                ui.horizontal(|ui| {
-                    let status = if task.completed { "✅" } else { "    " };
-                    let complete_button = ui.button(status).on_hover_text("Complete task");
-                    if complete_button.clicked() {
-                        task.completed = !task.completed;
-                    }
-                    ui.label(format!("{}: {}", task.title, task.description));
-
-                    let delete_button = ui.button("❌").on_hover_text("Delete task Permanently");
-                    if delete_button.clicked() {
-                        tasks_to_delete.push(task.index);
-                    }
-                });
-            }
-
-            for index in tasks_to_delete {
-                self.app_state.tasks.retain(|t| t.index != index);
-            }
-
-            if self.show_add_window {
-                egui::Window::new("Add New Task")
-                    .collapsible(false)
-                    .resizable(false)
-                    .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::new(0.0, 0.0))
-                    .show(ctx, |ui| {
-                        ui.horizontal(|ui| {
-                            ui.label("Title:");
-                            ui.text_edit_singleline(&mut self.draft_title);
-                        });
-
-                        ui.horizontal(|ui| {
-                            ui.label("Description:");
-                            ui.text_edit_singleline(&mut self.draft_desc);
-                        });
-
-                        ui.separator();
-
-                        let create_task_button = ui.button("Create Task");
-                        if create_task_button.clicked() {
-                            let new_task = todo_list::data::Task::new(
-                                &self.draft_title,
-                                &self.draft_desc,
-                                self.app_state.next_index,
-                            );
-                            self.app_state.tasks.push(new_task);
-                            self.app_state.next_index += 1;
-                            self.draft_title.clear();
-                            self.draft_desc.clear();
-                            self.show_add_window = false;
-                        }
-
-                        let cancel_button = ui.button("Cancel");
-                        if cancel_button.clicked() {
-                            self.draft_title.clear();
-                            self.draft_desc.clear();
-                            self.show_add_window = false;
-                        }
-                    });
-            }
-        });
-    }
-
-    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
-        // Save the state when the application exits
-        if let Err(e) = todo_list::persistence::save_state_file(&self.app_state) {
-            eprintln!("Failed to save state: {}", e);
-        }
-    }
 }
--- todo_list/src/lib.rs
+++ todo_list/src/lib.rs
@@ -1,15 +1,18 @@
-pub mod data;
-pub mod persistence;
 pub mod command_handler;
+pub mod data;
+pub mod gui_handler;
 pub mod input_handler;
+pub mod persistence;
 
 // unit testing
 #[cfg(test)]
 mod tests {
-    use crate::data::{AppState, Task};
-    use crate::persistence::{delete_state_file_name, load_state_from_file_name, save_state_to_file_name, state_file_name};
     use crate::command_handler::{Commands, handle_command};
-    
+    use crate::data::{AppState, Task};
+    use crate::persistence::{
+        delete_state_file_name, load_state_from_file_name, save_state_to_file_name, state_file_name,
+    };
+
     #[test]
     fn test_task_new() {
         let task = Task::new("Milk", "Groceries", 1);
@@ -20,7 +23,10 @@ mod tests {
 
     #[test]
     fn test_app_state_save_load_delete_file() {
-        let mut state = AppState { tasks: vec![], next_index: 0 };
+        let mut state = AppState {
+            tasks: vec![],
+            next_index: 0,
+        };
         let task = Task::new("Milk", "Groceries", state.next_index);
         state.tasks.push(task);
         state.next_index += 1;
@@ -39,8 +45,17 @@ mod tests {
 
     #[test]
     fn test_app_state_add_command() {
-        let mut state = AppState { tasks: vec![], next_index: 0 };
-        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        let mut state = AppState {
+            tasks: vec![],
+            next_index: 0,
+        };
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 1".to_string(),
+                description: "Description 1".to_string(),
+            },
+        );
         assert_eq!(state.tasks.len(), 1);
         assert_eq!(state.tasks[0].title, "Task 1");
         assert_eq!(state.tasks[0].description, "Description 1");
@@ -48,34 +63,73 @@ mod tests {
 
     #[test]
     fn test_app_state_complete_command() {
-        let mut state = AppState { tasks: vec![], next_index: 0 };
-        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        let mut state = AppState {
+            tasks: vec![],
+            next_index: 0,
+        };
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 1".to_string(),
+                description: "Description 1".to_string(),
+            },
+        );
         handle_command(&mut state, &Commands::Complete { index: 0 });
         assert_eq!(state.tasks[0].completed, true);
     }
 
     #[test]
     fn test_app_state_delete_command() {
-        let mut state = AppState { tasks: vec![], next_index: 0 };
-        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        let mut state = AppState {
+            tasks: vec![],
+            next_index: 0,
+        };
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 1".to_string(),
+                description: "Description 1".to_string(),
+            },
+        );
         handle_command(&mut state, &Commands::Delete { index: 0 });
         assert_eq!(state.tasks.len(), 0);
     }
 
     #[test]
     fn test_app_state_next_id() {
-        let mut state = AppState { tasks: vec![], next_index: 0 };
-        handle_command(&mut state, &Commands::Add { title: "Task 0".to_string(), description: "Description 0".to_string() });
+        let mut state = AppState {
+            tasks: vec![],
+            next_index: 0,
+        };
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 0".to_string(),
+                description: "Description 0".to_string(),
+            },
+        );
         let file_name = "test_state_2.json";
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        handle_command(&mut state, &Commands::Add { title: "Task 1".to_string(), description: "Description 1".to_string() });
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 1".to_string(),
+                description: "Description 1".to_string(),
+            },
+        );
         assert_eq!(state.tasks[1].index, 1);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        handle_command(&mut state, &Commands::Add { title: "Task 2".to_string(), description: "Description 2".to_string() });
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 2".to_string(),
+                description: "Description 2".to_string(),
+            },
+        );
         assert_eq!(state.tasks[2].index, 2);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
@@ -85,11 +139,16 @@ mod tests {
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
 
         state = load_state_from_file_name(file_name).expect("Failed to load state");
-        handle_command(&mut state, &Commands::Add { title: "Task 3".to_string(), description: "Description 3".to_string() });
+        handle_command(
+            &mut state,
+            &Commands::Add {
+                title: "Task 3".to_string(),
+                description: "Description 3".to_string(),
+            },
+        );
         assert_eq!(state.tasks[2].index, 3);
         save_state_to_file_name(&state, file_name).expect("Failed to save state");
-        
+
         delete_state_file_name(file_name).expect("Failed to delete state file");
     }
-
 }
